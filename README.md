67eeeca21818f135f6b75db03631a23f39aba131
Everton-Fernandes <e.everton.elf@gmail.com>
2023-08-22 21:03:04 -0300

    readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Primeiro app estudando ReactJs
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -4,15 +4,10 @@ import logo from '../../imagens/logo.svg'
 function Logo(){
     return(
         <div className='logo'>
-            <img
-                src={logo}
-                alt='logo'
-                className='logo-img'
-            ></img>
+            <img src={logo} alt='logo' className='logo-img'></img>
             <p><strong>T</strong>Books</p>
         </div>
     )
 }
-
-// só conseguimos utilizar esse componente em algum lugar por causa do export
+// só conseguimos utilizar esse componente em outro lugar por causa do export
 export default Logo;
